bdc3d1befe0cc7eaa84e4cb357ad08bc72dffba9
Amirul Irsyad <amirulirsyad157@gmail.com>
2026-04-04 08:13:24 +0800

    Added database tables

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+
+
+class User(UserMixin, db.Model):
+    __abstract__ = True
+
+    password_hash = db.Column(db.Text)
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable attribute!")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # Create String
+    def __repr__(self):
+        return "<Name %r>" % self.name
+
+
+class Admin(User):
+    __tablename__ = "admin"
+
+    idAdmin = db.Column(db.String(3), primary_key=True)
+    namaAdmin = db.Column(db.String(30), unique=True, index=True)
+
+
+class Calon(db.Model):
+    __tablename__ = "calon"
+
+    idCalon = db.Column(db.String(3), primary_key=True)
+    namaCalon = db.Column(db.String(20), unique=True, index=True)
+    gambar = db.Column(db.String(100), unique=True, index=True)
+    moto = db.Column(db.String(100), unique=True, index=True)
+    idAdmin = db.Column(db.String(3), db.ForeignKey("admin.idAdmin"))
+
+
+class Pengundi(User):
+    __tablename__ = "pengundi"
+
+    idPengundi = db.Column(db.String(4), primary_key=True)
+    namaPengundi = db.Column(db.String(30), unique=True, index=True)
+    idCalon = db.Column(db.String(3), db.ForeignKey("calon.idCalon"))
