910b864abbc1e5e55e6fcf29e595bd3cb6e4ced2
Amirul Irsyad <amirulirsyad157@gmail.com>
2026-05-12 17:30:31 +0800

    Preparing for deployment

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(24))
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "app/static/imej/"
+    UPLOAD_FOLDER = "app/static/imej/uploads/"
